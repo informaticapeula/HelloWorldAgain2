23b6baf26c5174568b49a2bfc5e413396db6c304
salva <informatica.peula@gmail.com>
2020-02-04 13:11:07 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorldAgain2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+            
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+            Console.Write("Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello world! My name is {0} and I am {1} years old." +
+                "I was born in {2}.", userName, userAge, currentYear - userAge);
+        }
+    }
+}
